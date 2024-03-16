411b37c067a0eed797f48c993b2680e38296c5e6
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-03-16 18:48:24 +0530

    server commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('./db')
 const app = express();
 const Person = require('./models/person');
+const MenuItem = require('./models/menuitem');
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
@@ -26,6 +27,78 @@ app.post('/person', async (req,res)=>{
     }
     
 })
+
+app.post('/menuitem', async (req,res)=>{
+
+
+    try {
+        const data = req.body
+        const newMenuItem = new MenuItem(data);
+
+        const response = await newMenuItem.save()
+        console.log('data saved');
+        res.status(200).json(response)
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+    
+})
+
+
+app.get('/person', async (req,res) => {
+    try {
+
+        const data = await Person.find()
+        console.log('Data fetched successfully');
+        res.status(200).json(data)
+
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+        
+        
+    }
+})
+
+app.get('/menuItem', async (req,res) => {
+    try {
+
+        const data = await MenuItem.find()
+        console.log('Data fetched successfully');
+        res.status(200).json(data)
+
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+        
+        
+    }
+})
+
+
+app.get('/person/:workType', async (req,res) => {
+    try {
+
+        const workType = req.params.workType;
+        if (workType == 'chef' || workType == 'manager' || workType == 'waiter'){
+            const response = await Person.find({work: workType})
+            console.log('response fetched successfully');
+            res.status(200).json(response)
+        }
+        else{
+            res.status(404).json({error: 'Invalid work type'});
+        }  
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+     
+    }
+})
+
 
    
 app.listen(3000, () => {
