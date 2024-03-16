a2a5e9b14ad09d970cc90c91296b3a2ee0187136
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-03-16 15:54:32 +0530

    new commit

--- server.js
+++ server.js
@@ -2,31 +2,32 @@ const express = require('express')
 const db = require('./db')
 const app = express();
 const Person = require('./models/person');
-
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
-
 
 app.get('/', (req,res) => {
     res.send('Welcome to my HOTEL, how can i help You?')
 })
 
-app.post('/person',(req,res)=>{
-    const data = req.body
+app.post('/person', async (req,res)=>{
 
-    const newPerson = new Person();
-    newPerson.name = data.name;
-    newPerson.work = data.work;
-    newPerson.mobile = data.mobile;
-    newPerson.email = data.email;
-    newPerson.salary = data.salary;
-    newPerson.age = data.age;
-    newPerson.address = data.address;
 
+    try {
+        const data = req.body
+        const newPerson = new Person(data);
+
+        const response = await newPerson.save()
+        console.log('data saved');
+        res.status(200).json(response)
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+    
 })
 
-
-
+   
 app.listen(3000, () => {
     console.log("Server is running at port 3000");
 })
