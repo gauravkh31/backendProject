315bdd7f1d88edd4b45568ff051c474785c031fb
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-03-17 06:12:04 +0530

    delete operation

--- routes/personroutes.js
+++ routes/personroutes.js
@@ -55,13 +55,13 @@ router.get('/:workType', async (req,res) => {
     }
 })
 
-router.get('/:id', async (req,res) => {
+router.put('/:id', async (req,res) => {
     try {
 
         const personid = req.params.id;
         const updatedpersondata = req.body;
 
-        const response = await person.findByIdAndUpdate(personid, updatedpersondata, {
+        const response = await Person.findByIdAndUpdate(personid, updatedpersondata, {
             new: true,
             runValidators: true
         })
@@ -71,6 +71,27 @@ router.get('/:id', async (req,res) => {
         
             console.log('data updated successfully');
             res.status(200).json(response)
+        } 
+        catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+     
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try {
+
+        const personid = req.params.id;
+       
+
+        const response = await Person.findByIdAndRemove(personid)
+        if(!response){
+            return res.status(404).json({error: 'person not found'})
+        }
+        
+            console.log("data deleted successfully!");
+            res.status(200).json({message: 'data deleted successfully'})
         } 
         catch (err) {
         console.log(err);
